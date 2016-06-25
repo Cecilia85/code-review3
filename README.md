848684ca20a12b6ff8c43e530376d1b02ffa537c
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-06-25 09:27:24 -0700

    Add initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+SPECS
+
+BEHAVIOUR                      INPUT                           OUTPUT
+User enters three fruits      banana, pineapple, carrot       You had a bapica milkshake!
+-----------------------------------------------------------------------------------------
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,19 @@
-// Business logic or Back end//
 
-var numerology= function(x) {
+var milkshake = function(fruit1, fruit2, fruit3) {
+var slice= fruit1.slice( 2 )+fruit2.slice(2)+fruit3.slice(2);
+var fruitscombined = slice.join(" ");
+slice.forEach(function(fruitscombined){
 
+  alert("You had a" + fruitscombined + "milkshake");
 
-        if(x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7] <= 30){
-        return("Your number is less than 30.Youre a happy person, you like to share and hang out with friends");
-      } else if(x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7] > 30 && x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7] <=60 );{
-        return("Your number is between 30 and 60.Youre a sweet person, you always put others needs before yours");
-      } else if(x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7] > 60 && x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7]  <= 100 );{
-        return("your number is between 60 and 100.You like to get into trouble");
-      } else {
-        return("Enter a date");
-      }
+});
+};
+$(document).ready(function() {
+  $("form#blanks").submit(function(event) {
+    event.preventDefault();
+    var fruit1 = toUpperCase()($("input#fruit1").val());
+    var fruit2 = toUpperCase()($("input#fruit2").val());
+    var fruit3 = toUpperCase()($("input#fruit3").val());
 
-    };
-
-//Front-end logic//
-    $(function() {
-      $("form#blanks").submit(function(event) {
-        event.preventDefault();
-        var date = ($("input#date").val());
-        var result =numerology(date);
-        // $("#output").text(return);
-      });
-    });
+  });
+});
