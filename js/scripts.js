6998983a356cc9bab13c7d27f7be1747c3d4bbad
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-06-25 17:19:47 -0700

    Changed files

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 
-var milkshake = function(fruit1, fruit2, fruit3) {
-var slice= fruit1.slice( 2 )+fruit2.slice(2)+fruit3.slice(2);
-var fruitscombined = slice.join(" ");
-slice.forEach(function(fruitscombined){
-
-  alert("You had a" + fruitscombined + "milkshake");
-
+var fruits= function(fruit1,fruit2,fruit3){
+var fruitSliced=fruit1.slice(2)+fruit2.slice(2)+fruit3.slice(2);
+var fruitsCombined= fruitSliced.map(function(milkshake){
+  alert ("You had a "+ milkshake.join(''))+"milkshake!";
 });
+
 };
+
+
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
     event.preventDefault();
     var fruit1 = toUpperCase()($("input#fruit1").val());
-    var fruit2 = toUpperCase()($("input#fruit2").val());
-    var fruit3 = toUpperCase()($("input#fruit3").val());
+    var fruit2 = toUpperCase()($("input#fruit1").val());
+    var fruit3 = toUpperCase()($("input#fruit1").val());
+    var fruits= milkshake(fruit1,fruit2,fruit3);
+    // $("#output").text(fruits);
 
   });
 });
