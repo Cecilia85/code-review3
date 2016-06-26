7ca4f479af4953e1766eb0e10ee480a7253e8cf6
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-06-25 18:12:31 -0700

    Changed js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
-
-var fruits= function(fruit1,fruit2,fruit3){
-var fruitSliced=fruit1.slice(2)+fruit2.slice(2)+fruit3.slice(2);
-var milkshake= fruitSliced.map(function(fruitsCombined){
-  alert ("You had a "+ fruitsCombined.join('')+"milkshake!");
-});
+//
+// var fruits= function(fruit1,fruit2,fruit3){
+// var fruitSliced=fruit1.slice(2)+fruit2.slice(2)+fruit3.slice(2);
+// var milkshake= fruitSliced.map(function(fruitsCombined){
+//   return "You had a "+ fruitsCombined.join('')+"milkshake!";
+// });
+//
+// };
+var milkshake = function(fruits) {
+var fruits=[];
+var fruitSliced=fruits[0].slice(2)+fruits[1].slice(2)+fruits[2].slice(2);
+  return("You had a "+ fruitSliced+" milkshake!");
 
 };
+
 
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
@@ -13,6 +20,10 @@ $(document).ready(function() {
     var fruit1 = ($("input#fruit1").val()).toUpperCase();
     var fruit2 = ($("input#fruit2").val()).toUpperCase();
     var fruit3 = ($("input#fruit3").val()).toUpperCase();
+    var result = milkshake(fruits);
+    // $("#output").text(result);
+    alert(result);
+
   });
 
 
